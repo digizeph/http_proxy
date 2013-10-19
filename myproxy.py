6eb8b04bd06a0a33d85cf8bb585a36f169a60751
Mingwei <danielzhang0212@gmail.com>
2013-10-19 16:20:52 -0700

    add more test cases. the proxy is basically finished.

--- myproxy.py
+++ myproxy.py
@@ -13,33 +13,56 @@ def processConn(conn,addr):
 	for d in lst:
 		if re.search("^Host",d):
 			host = d.split(' ')[1]
-	#print "host",host
 	if not host=='':
 		c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		try:
 			c.connect((host, 80))
 		except:
-			print "connection to %s failed."%h
-			conn.send( "connection to %s failed."%h)
+			print "connection to %s failed."%host
+			conn.send( "connection to %s failed."%host)
 			conn.close()
 			return
 		c.send(request)
 		cdata = ""
+		FIRST=True
+		content_size=-1
+		CONTENT=False
 		while 1:
 			try:
-				line = c.recv(1024)
+				line = c.recv(4096)
+				if FIRST:
+					strlist=(line.split("\r\n\r\n"))
+					header=strlist[0].split("\r\n")
+					for h in header:
+						if h.startswith("Content-Length"):
+							content_size=int(h.split(" ")[1])
+					if content_size>=0:
+						content_size-=len(strlist[1])
+						CONTENT=True
+				else:
+					if CONTENT:
+						content_size -= len(line)
 				cdata += str(line)
+				if CONTENT and content_size<=0:
+					break
+				elif re.search("</HTML>",line,re.IGNORECASE):
+						break
+
+				'''
 				if re.search("</HTML>",line,re.IGNORECASE):
-					#					print "FOUND /HTML"
+					#print "RECV: END OF HTML"
 					break
 				if len(line)==0:
+					#print "RECV: EMPTY"
 					break
-				#print line
-			except :
+				'''
+			except Exception,e:
+				print e
 				break
 		c.close()
 		conn.send(cdata)
 	conn.close()
+	print "finish"
 
 
 if __name__=="__main__":
@@ -48,6 +71,8 @@ if __name__=="__main__":
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((HOST, PORT))
 	s.listen(1)
+	print s.gettimeout()
 	while 1:
+		print "start" 	# tested, multi-thread is correct.
 		conn, addr = s.accept()
 		thread.start_new_thread(processConn,(conn,addr))
