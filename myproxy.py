93a0d26f07ccfbcc868d72ced4076cb6c9309dd0
Mingwei <danielzhang0212@gmail.com>
2013-10-22 11:15:26 -0700

    clean up code, added processing for chunked.

--- myproxy.py
+++ myproxy.py
@@ -5,8 +5,10 @@ import thread
 import sys, os
 
 def processConn(conn,addr):
-	request = conn.recv(1024)
-	new_request_list = []
+	request = conn.recv(8092)
+	print "######## REQUEST ########"
+	print request
+	print "######## END OF REQUEST ########"
 	host = ''
 	get=''
 	redirect=''
@@ -19,13 +21,8 @@ def processConn(conn,addr):
 			host = d.split(' ')[1]
 		elif re.search("^GET",d):
 			get = d.split(' ')[1]
-		elif re.search("Accept-Encoding",d):
-			continue
-		new_request_list.append(d)
-	new_request="\r\n".join(new_request_list)
 
-	print "request content from ",host,get
-	print new_request_list
+	#print "request content from ",host,get
 	if not host=='':
 		c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		try:
@@ -35,14 +32,18 @@ def processConn(conn,addr):
 			conn.send( "connection to %s failed."%host)
 			conn.close()
 			return
-		c.send(new_request)
+		c.settimeout(3)
+		num_sent = c.send(request)
+		print "******** REQUEST SENT TO SERVER %d ********"%num_sent
 		cdata = ""
 		FIRST=True
 		content_size=-1
 		CONTENT=False
+		HTML=False
+		CHUNKED=False
 		while 1:
 			try:
-				line = c.recv(4096)
+				line = c.recv(8092)
 				if line=="":
 					print "   EMPTY LINE"
 					return
@@ -50,71 +51,46 @@ def processConn(conn,addr):
 				if FIRST:
 					strlist=(line.split("\r\n\r\n"))
 					header=strlist[0].split("\r\n")
+					content=""
+					print "######## RESPONSE ########"
 					print header
+					print "######## END OF RESPONSE ########"
 					for h in header:
 						if h.startswith("Content-Length"):
 							content_size=int(h.split(" ")[1])
-						# REDIRECTION
-						if re.search("Location",h):
-							redirect = h.split(" ")[1].lower()
-							rhost=redirect.lstrip("http://")
-							resource="/"+"/".join(rhost.split("/")[1:])
-							rhost=rhost.split("/")[0]
-							c.close()
-							try:
-								c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-								c.connect((rhost, 80))
-								new_request_lst=[]
-								for d in lst:
-									ld=d.lower()
-									new=''
-									if re.search("^host",ld):
-										new = "Host: %s"%rhost
-									elif re.search("^get",ld):
-										new = "%s %s %s"%(d.split(" ")[0],resource,d.split(" ")[2])
-									elif re.search("encoding"):
-										print "disable encoding",d
-										continue
-									else:
-										new = d
-									new_request_lst.append(new)
-								new_request="\r\n".join(new_request_lst)
-								c.send(new_request)
-								cdata = ""
-								FIRST=True
-								content_size=-1
-								CONTENT=False
-							except Exception as e:
-								exc_type, exc_obj, exc_tb = sys.exc_info()
-								fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-								print(exc_type, fname, exc_tb.tb_lineno)
-								print "[2] connection to %s failed."%rhost
-								conn.send( "connection to %s failed."%rhost)
-								conn.close()
-								return
-							continue
-							
-					if content_size>0:
-						try:
-							content_size-=len(strlist[1])
+							content=strlist[1]
 							CONTENT=True
-						except:
-							pass
+						if h.startswith("Content-Type: text/html"):
+							HTML=True
+						if h.startswith("Transfer-Encoding: chunked"):
+							CHUNKED=True
+					FIRST=False
 				else:
-					if CONTENT:
-						content_size -= len(line)
-				FIRST=False
+					content=line
+
 				cdata += str(line)
-				if CONTENT and content_size<=0:
-					break
-				elif re.search("</HTML>",line,re.IGNORECASE):
+
+				if CONTENT:
+					content_size-=len(content)
+					if content_size<=0:
 						break
+				elif HTML and re.search("</HTML>",line,re.IGNORECASE):
+					break
+				elif CHUNKED:
+					if re.search("\r\n0\r\n\r\n",line):
+						break
+
+			except socket.timeout:
+				c.close()
+				conn.send(cdata)
+				print "TIMEOUT ! ERROR !"
+				break
 
 			except Exception as e:
 				exc_type, exc_obj, exc_tb = sys.exc_info()
 				fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
 				print(exc_type, fname, exc_tb.tb_lineno)
-				#print e
+				print e
 				break
 		c.close()
 		conn.send(cdata)
